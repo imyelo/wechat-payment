5fa6f4fd347ca3cd75ab91e717115b99976242fa
yelo <zhihuzeye@gmail.com>
2014-03-13 15:00:20 +0800

    add test

---
+++ test/common.js
@@ -0,0 +1,48 @@
+var chai = require('chai');
+var expect = chai.expect;
+var muk = require('muk');
+
+var Payment = require('../lib/payment').Payment;
+var common = require('../lib/common');
+
+describe('case', function () {
+  describe('getPayRequest', function () {
+    before(function () {
+      muk(common, 'getTimestamp', function () {
+        return "189026618";
+      }),
+      muk(common, 'getNonceStr', function () {
+        return "adssdasssd13d";
+      });
+    });
+    after(function () {
+      muk.restore();
+    });
+    it('should work', function () {
+      var order = {
+        bank_type: 'WX',
+        fee_type: '1',
+        body: 'XXX',
+        input_charset: 'GBK',
+        partner: '1900000109',
+        total_fee: '1',
+        spbill_create_ip: '127.0.0.1',
+        out_trade_no: '16642817866003386000',
+        notify_url: 'http://www.qq.com'
+      };
+      var payment = new Payment(
+        'wxf8b4f85f3a794e77',
+        '2Wozy2aksie1puXUBpWD8oZxiD1DfQuEaiC7KcRATv1Ino3mdopKaPGQQ7TtkNySuAmCaDCrw4xhPY5qKTBl7Fzm0RgR3c0WaVYIXZARsxzHV2x7iwPPzOz94dnwPWSn',
+        '8934e7d15453e97507ef794cf7b0519d'
+        );
+      expect(payment.getPayRequest(order)).to.be.deep.equal({
+        'appId': 'wxf8b4f85f3a794e77',
+        'timeStamp': '189026618',
+        'nonceStr': 'adssdasssd13d',
+        'package': 'bank_type=WX&body=XXX&fee_type=1&input_charset=GBK&notify_url=http%3a%2f%2fwww.qq.com&out_trade_no=16642817866003386000&partner=1900000109&spbill_create_ip=127.0.0.1&total_fee=1&sign=BEEF37AD19575D92E191C1E4B1474CA9',
+        'paySign': '7717231c335a05165b1874658306fa431fe9a0de',
+        'signType': 'SHA1'
+      });
+    });
+  });
+});
